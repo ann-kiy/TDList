fc2f626a54374e6c280a07f6accf58cc29aa55f0
ann-kiy <ann-kiy@mail.ru>
2017-04-22 20:59:42 +0700

    Implemented sorting and use List

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Collections;
 
 namespace ToDoList
 {
@@ -28,6 +28,15 @@ namespace ToDoList
         }
         ArrayList textBox = new ArrayList();
         int i = 0, tab = 1;
+        public class Rek
+        {
+            public DateTime date;
+            public string text;
+        }
+        List<Rek> usr = new List<Rek>();
+        Rek u1 = new Rek();
+
+
         void AddTextBox(int i, ArrayList textBox, int h, int l)
         {
 
@@ -46,23 +55,71 @@ namespace ToDoList
 
             FileStream file = new FileStream("dataBase.txt", FileMode.Append, FileAccess.Write);
             StreamWriter writer = new StreamWriter(file);
-            writer.Write(text + "/");
-            writer.Write(date);
 
-            writer.Write('\n');
+            writer.WriteLine(text+"/"+date);
             writer.Close();
 
         }
 
+        void FillArr()
+        {
+            FileStream file = new FileStream("dataBase.txt", FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(file);
+            string str;
+
+            while (!read.EndOfStream) //Цикл длиться пока не будет достигнут конец файла
+            {
+                str = read.ReadLine();
+                u1.date = new DateTime(int.Parse((str.Split('/')[1]).Split('.')[2].Split(' ')[0]), int.Parse((str.Split('/')[1]).Split('.')[1]), int.Parse((str.Split('/')[1]).Split('.')[0]));
+                u1.text = str.Split('/')[0];
+                usr.Add(u1);
+                u1 = new Rek();
+
+                i++;
+            }
+
+
+            read.Close();
+            usr = usr.OrderBy(u1 => u1.date).ToList();
+
+
+        }
+        // void DelRek()
+
+
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+
             FileWrite(textBox1.Text, DatePicker1.Text.ToString());
 
             AddTextBox(i, textBox, 1, tab);
             ((TextBox)textBox[i]).Text = textBox1.Text;
             tab += 30;
             i++;
+        }
+
+        private void MyGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            FillArr();
+            int j = 0;
+            foreach (Rek i in usr)
+            {
+
+                AddTextBox(j, textBox, 1, tab);
+                tab += 30;
+                ((TextBox)textBox[j]).Text = i.text;
+                j++;
+            }
+
 
         }
+
+
     }
+
+
 }
+
