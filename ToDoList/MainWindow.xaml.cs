9b7e2ad97b2a85707227c763639481379546affa
ann-kiy <ann-kiy@mail.ru>
2017-04-22 19:51:45 +0700

    Reading and displaying the database

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,44 @@ namespace ToDoList
         public MainWindow()
         {
             InitializeComponent();
+        }
+        ArrayList textBox = new ArrayList();
+        int i = 0, tab = 1;
+        void AddTextBox(int i, ArrayList textBox, int h, int l)
+        {
+
+            textBox.Add(new TextBox());
+            ((TextBox)textBox[i]).HorizontalAlignment = HorizontalAlignment.Left;
+            ((TextBox)textBox[i]).Height = 20;
+            ((TextBox)textBox[i]).Width = 346;
+            ((TextBox)textBox[i]).VerticalAlignment = VerticalAlignment.Top;
+            ((TextBox)textBox[i]).TextWrapping = TextWrapping.Wrap;
+            ((TextBox)textBox[i]).Margin = new Thickness(h, l, 0, 0);
+            MyGrid.Children.Add(((TextBox)textBox[i]));
+
+        }
+        void FileWrite(string text, string date)
+        {
+
+            FileStream file = new FileStream("dataBase.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file);
+            writer.Write(text + "/");
+            writer.Write(date);
+
+            writer.Write('\n');
+            writer.Close();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileWrite(textBox1.Text, DatePicker1.Text.ToString());
+
+            AddTextBox(i, textBox, 1, tab);
+            ((TextBox)textBox[i]).Text = textBox1.Text;
+            tab += 30;
+            i++;
+
         }
     }
 }
