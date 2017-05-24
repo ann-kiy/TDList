709e5bf8f0aee01dc996803623bf9c4493e21b64
aleks5323 <crazyfreind@mail.ru>
2017-05-24 06:54:16 +0300

    Fix: Added results displaying in ReedOfFileInArrayTest

--- ToDoListTests/MainWindowTests.cs
+++ ToDoListTests/MainWindowTests.cs
@@ -16,7 +16,7 @@ namespace ToDoList.Tests
         static Random rn = new Random();
         static int RandomNumber = rn.Next(0, 5000);
 
-        string date = "10.10.2017 00:00:00";
+        string date = "10.10.2017 0:00:00";
 
         [TestMethod()]
         public void FileExistenceTest()
@@ -54,7 +54,7 @@ namespace ToDoList.Tests
             MessageBox.Show(date);
             MessageBox.Show(tr[t.coutRecords - 1].date.ToString());*/
             if (tr[t.coutRecords - 1].text.ToString() != RandomNumber.ToString()) Assert.Fail("Error reading from file: wrong text");
-            if (tr[t.coutRecords - 1].date.ToString() != date) Assert.Fail("Error reading from file: wrong date");
+            if (tr[t.coutRecords - 1].date.ToString() != date) Assert.Fail("Error reading from file: wrong date. Expected: " + date + " but recieved: " + tr[t.coutRecords - 1].date.ToString());
         }
     }
 }
